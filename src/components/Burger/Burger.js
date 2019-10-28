325264c40033474fbcb2c1553febc069a00e8f2e
klupzor <lopezgand@gmail.com>
2019-10-28 17:56:20 -0500

    adding massage for empty ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,19 @@ import clasees from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burguer = (props) => {
-    const trasnformedIngredients = Object.keys(props.ingredients)
+    let trasnformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
             return <BurgerIngredient key={igKey + i} type={igKey} />;
         });
-    });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+
+    if (trasnformedIngredients.length === 0) {
+        trasnformedIngredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <div className={clasees.Burger}>
             <BurgerIngredient type="bread-top"/>
